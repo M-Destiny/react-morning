158e5be885eadc3baaf3dd7530f83577fb22b341
m-destiny <mehulr2801@gmail.com>
2023-03-26 10:26:28 +0530

    Mary on a cross

--- src/CrudService/CrudService.jsx
+++ src/CrudService/CrudService.jsx
@@ -12,10 +12,57 @@ class CrudService {
             console.log(error);
         }
     }
-    updateData() { }
-    deleteData() { }
-    async insertData(values) {
 
+
+    // TODO: add a function to update the data in existing row
+    // TODO: add a feild to add the data in existing row
+    // add a function to update the data in existing row
+    async updateData(values) {
+        // add code to update the data
+        await fetch(ApiPath.FirebaseApi, {
+            method: 'PUT',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(values)
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log('Success:', data);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            }
+            );
+
+
+    }
+    // TODO: add a function to delete the data in existing row 
+    // TODO: add a button to delete the data in existing row
+    // add a function to delete the data in existing row
+    async deleteData(values) {
+        await fetch(ApiPath.FirebaseApi, {
+            method: 'DELETE',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(values)
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log('Success:', data);
+            })
+
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    }
+
+
+
+    async insertData(values) {
         await fetch(ApiPath.FirebaseApi, {
             method: 'POST',
             mode: 'cors',
